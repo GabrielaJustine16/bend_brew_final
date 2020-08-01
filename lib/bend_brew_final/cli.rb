92a5c0ca6cb2cec712c15fbda13bd9f2d4107203
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-01 04:53:53 -0700

    adds first 3 methods to cli

--- lib/bend_brew_final/cli.rb
+++ lib/bend_brew_final/cli.rb
@@ -4,8 +4,23 @@ require "nokogiri"
 class BendBrewFinal::CLI
     
     def start
+        puts "Welcome to the Breweries of Bend , Oregon Gem\n"
+        self.list_breweries
+        self.menu
+
+    end 
+
+       def self.list_breweries
+        puts "Listed are the wonderful breweries located in Bend, Oregon\n"
+        #breweries = BendBrewFinal::Scraper
+        breweries.each_with_index(1) do |brewery, index|
+            puts "#{index}.#{brewery.name}-#{brewery.address}"        
+         end 
+
+       def self.menu
+        puts "Select the number next to the brewey you want more information on."
+        input=gets.strip.downcase
         
 
-       
-    end 
+       end
 end
