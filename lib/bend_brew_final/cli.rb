568b73663cc62c45578c70745c2bd3bd357e3309
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-01 05:02:23 -0700

    adds all method

--- lib/bend_brew_final/cli.rb
+++ lib/bend_brew_final/cli.rb
@@ -3,24 +3,34 @@ require "nokogiri"
 
 class BendBrewFinal::CLI
     
+    @@all=[]
+    
     def start
         puts "Welcome to the Breweries of Bend , Oregon Gem\n"
+        #breweries= scrape breweries
+        breweries.each {|brewery| @@all << brewery}
         self.list_breweries
         self.menu
 
     end 
 
+    def self.all 
+        @@all
+    end 
+
        def self.list_breweries
         puts "Listed are the wonderful breweries located in Bend, Oregon\n"
         #breweries = BendBrewFinal::Scraper
-        breweries.each_with_index(1) do |brewery, index|
+        all.each_with_index(1) do |brewery, index|
             puts "#{index}.#{brewery.name}-#{brewery.address}"        
          end 
 
        def self.menu
         puts "Select the number next to the brewey you want more information on."
         input=gets.strip.downcase
+        #scrape web for more info 
         
+
 
        end
 end
