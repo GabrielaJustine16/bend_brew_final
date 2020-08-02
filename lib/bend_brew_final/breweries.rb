05c4b6b9a00ec5086f2c1e486dda6ff80a275274
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-01 22:15:56 -0700

    adds find brewery by url method

---
+++ lib/bend_brew_final/breweries.rb
@@ -0,0 +1,30 @@
+class Brewery
+
+
+attr_accessor :number , :name , :address , :url , :beers
+
+  @@all= []
+
+  def initialize (number = 1, name = "", address = "", url = "", beers = "")
+
+    @number = number 
+    @name = name
+    @address = address
+    @url = url
+    @beers = []
+    @@all<<self
+  end
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.find_brewery_by_number(number)
+    Brewery.all.find {|brewery| brewery.number == number}
+  end 
+
+  def self.find_brewery_by_url(url)
+    Brewery.all.find {|brewey| brewery.url == url
+  end
+
+end 
