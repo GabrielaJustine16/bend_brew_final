7c2cbb34667600abb0a0f4d9eede979c39537174
GabrielaJustine16 <johnsongabriela12@gmail.com>
2020-08-07 13:33:51 -0700

    fixes the run folder

--- lib/bend_brew_final/cli.rb
+++ lib/bend_brew_final/cli.rb
@@ -4,7 +4,6 @@ require "nokogiri"
 
 class CLI
     
-    #@@all=[]
     
     def start
         puts "Welcome to the Breweries of Bend , Oregon Gem ! \n"
@@ -12,30 +11,23 @@ class CLI
         get_breweries
         list_breweries 
         
-        #breweries= scrape breweries
-        #breweries.each{|brewery| @@all << brewery}
-       # self.list_breweries
-        #self.menu
+        
     end 
 
-    #def self.all 
-        #@@all
-    #end 
+    
     def get_breweries
         Scraper.new.scrape_breweries
     end 
 
     def list_breweries
         puts "Listed are the wonderful breweries located in Bend, Oregon ! \n"
-        #breweries = BendBrewFinal::Scraper
-        #all.each_with_index(1) do |brewery, index|
-            #puts "#{index}.#{brewery.name}-#{brewery.address}"  
-        #end      
+            
         Brewery.all.each do |brewery|
             puts "#{brewery.number}. #{brewery.name}-#{brewery.address}"
         end 
 
         menu 
+        
     end 
 
     def  list_beers(brewery)
@@ -87,10 +79,7 @@ class CLI
         puts "Enter the number next to the brewery youd like more information on!"
 
         input=gets.strip
-        #index=input.to_i-1
-        #scrape web for more info 
-        #beer advocate url is brewery.url
-        #if index > 0 && index < @@all.size
+        
         if input.to_i >= 1 && input.to_i <= Brewery.all.length
 
             list_beers(Brewery.find_brewery_by_number(input.to_i))
